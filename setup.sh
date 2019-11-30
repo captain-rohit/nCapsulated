54b425ed42fc97727624307b653ec2646ebfbf52
erroneous_nerd <35062208+iamRohit11@users.noreply.github.com>
2019-11-30 17:08:20 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,32 @@
+# var="sudo apt-get update"
+# eval $var
+
+# var="sudo apt-get install python"
+# eval $var
+
+var="python -m venv ."
+eval $var
+
+var="conda deactivate"
+eval $var
+
+var="source bin/activate"
+eval $var
+
+var="pip install django"
+eval $var
+
+var="pip install django-db-file-storage"
+eval $var
+
+var="cd src/"
+eval $var
+
+var="python manage.py makemigrations"
+eval $var
+
+var="python manage.py migrate"
+eval $var
+
+var="python manage.py runserver"
+eval $var
